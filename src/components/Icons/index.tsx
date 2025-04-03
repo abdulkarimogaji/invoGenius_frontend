5b4f86c559b47cebbdb9e0464274e1cedcaa7be1
Abdulkarim <abdulkarimogaji001@gmail.com>
2025-04-03 07:03:38 +0100

    implement logout

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,7 +1,9 @@
 import AtSymbolIcon from "./AtSymbolIcon";
+import Bell02Icon from "./Bell02Icon";
 import CalendarIcon from "./CalendarIcon";
 import CoinsHandIcon from "./CoinsHandIcon";
 import CheckCircleIcon from "./CheckCircleIcon";
+import ChevronRightIcon from "./ChevronRightIcon";
 import ClockIcon from "./ClockIcon";
 import CoinsStacked02Icon from "./CoinsStacked02Icon";
 import CoinsSwap02Icon from "./CoinsSwap02Icon";
@@ -13,23 +15,29 @@ import FileCheck03Icon from "./FileCheck03Icon";
 import HelpCircleIcon from "./HelpCircleIcon";
 import HomeLineIcon from "./HomeLineIcon";
 import Key01Icon from "./Key01Icon";
+import LogOut01Icon from "./LogOut01Icon";
+import Mail01Icon from "./Mail01Icon";
 import Mail02Icon from "./Mail02Icon";
 import MessageChatSquareIcon from "./MessageChatSquareIcon";
 import PhoneCall01Icon from "./PhoneCall01Icon";
 import PlusCircleIcon from "./PlusCircleIcon";
 import ReceiptCheckIcon from "./ReceiptCheckIcon";
+import RefreshCCW04Icon from "./RefreshCCW04Icon";
 import SearchLgIcon from "./SearchLgIcon";
 import Settings02Icon from "./Settings02Icon";
 import Share05Icon from "./Share05Icon";
 import SlashCircle1Icon from "./SlashCircle01Icon";
+import User01Icon from "./User01Icon";
 import Users01Icon from "./Users01Icon";
 import VideoRecorderIcon from "./VideoRecorderIcon";
 import XCircleIcon from "./XCircleIcon";
 
 export {
   AtSymbolIcon,
+  Bell02Icon,
   CalendarIcon,
   CheckCircleIcon,
+  ChevronRightIcon,
   ClockIcon,
   CoinsHandIcon,
   CoinsStacked02Icon,
@@ -42,15 +50,19 @@ export {
   HelpCircleIcon,
   HomeLineIcon,
   Key01Icon,
+  LogOut01Icon,
+  Mail01Icon,
   Mail02Icon,
   MessageChatSquareIcon,
   PhoneCall01Icon,
   PlusCircleIcon,
   ReceiptCheckIcon,
+  RefreshCCW04Icon,
   SearchLgIcon,
   Share05Icon,
   Settings02Icon,
   SlashCircle1Icon,
+  User01Icon,
   Users01Icon,
   VideoRecorderIcon,
   XCircleIcon,
